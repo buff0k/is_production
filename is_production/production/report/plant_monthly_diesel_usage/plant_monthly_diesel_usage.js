7118ee12fc5f5db7c4e3332cb03e3b2e2d2966fe
buff0k <buff0k@gmail.com>
2024-10-27 18:52:02 +0000

    modified:   is_production/production/doctype/daily_diesel_entries/daily_diesel_entries.json 	modified:   is_production/production/report/plant_monthly_diesel_usage/plant_monthly_diesel_usage.js 	modified:   is_production/production/report/plant_monthly_diesel_usage/plant_monthly_diesel_usage.py Fixed Diesel Usage Report

--- is_production/production/report/plant_monthly_diesel_usage/plant_monthly_diesel_usage.js
+++ is_production/production/report/plant_monthly_diesel_usage/plant_monthly_diesel_usage.js
@@ -4,55 +4,25 @@
 frappe.query_reports["Plant Monthly Diesel Usage"] = {
     "filters": [
         {
-            "fieldname": "site",
-            "label": __("Site"),
+            "fieldname": "location",
+            "label": "Site",
             "fieldtype": "Link",
-            "options": "Location",  // Adjust "Location" to the relevant Doctype for your sites if different
-            "reqd": 1,
-            "default": "",  // Optionally set a default value here
-            "get_query": function() {
-                return {
-                    filters: {
-                        "is_group": 0  // Adjust or add filters specific to your setup if necessary
-                    }
-                };
-            }
+            "options": "Location",
+            "reqd": 1
         },
         {
             "fieldname": "from_date",
-            "label": __("From Date"),
+            "label": "From Date",
             "fieldtype": "Date",
             "default": frappe.datetime.month_start(),
             "reqd": 1
         },
         {
             "fieldname": "to_date",
-            "label": __("To Date"),
+            "label": "To Date",
             "fieldtype": "Date",
             "default": frappe.datetime.month_end(),
             "reqd": 1
         }
-    ],
-
-    "onload": function(report) {
-        report.page.add_inner_button(__('Fetch Records'), function() {
-            let filters = report.get_values();
-            frappe.call({
-                method: "is_production.production.report.plant_monthly_diesel_usage.plant_monthly_diesel_usage.fetch_records",
-                args: {
-                    "site": filters.site,
-                    "from_date": filters.from_date,
-                    "to_date": filters.to_date
-                },
-                callback: function(response) {
-                    if(response.message) {
-                        report.data = response.message;
-                        report.refresh();
-                    } else {
-                        frappe.msgprint(__('No records found for the selected criteria.'));
-                    }
-                }
-            });
-        });
-    }
+    ]
 };
--- is_production/production/report/plant_monthly_diesel_usage/plant_monthly_diesel_usage.py
+++ is_production/production/report/plant_monthly_diesel_usage/plant_monthly_diesel_usage.py
@@ -4,17 +4,35 @@
 import frappe
 
 def execute(filters=None):
-	columns, data = [], []
-	return columns, data
+    if not filters:
+        filters = {}
 
-@frappe.whitelist()
-def fetch_records(location, from_date, to_date):
-    records = frappe.get_all(
-        "Daily Diesel Sheet",
-        filters={
-            "location": location,
-            "daily_sheet_date": ["between", [from_date, to_date]]
-        },
-        fields=["name", "location", "daily_sheet_date", "litres_dispensed", "other_fields_you_need"]
-    )
-    return records
+    if not filters.get("location") or not filters.get("from_date") or not filters.get("to_date"):
+        frappe.throw("Please select a location and date range to run the report.")
+
+    columns = [
+        {"label": "Document ID", "fieldname": "name", "fieldtype": "Link", "options": "Daily Diesel Sheet", "width": 100},
+        {"label": "Location", "fieldname": "location", "fieldtype": "Link", "options": "Location", "width": 120},
+        {"label": "Date", "fieldname": "daily_sheet_date", "fieldtype": "Date", "width": 100},
+        {"label": "Litres Dispensed", "fieldname": "litres_issued_equipment", "fieldtype": "Float", "width": 120},
+        # Add other necessary columns here
+    ]
+
+    conditions = "WHERE 1=1"
+    
+    if filters.get("location"):
+        conditions += " AND location = %(location)s"
+    if filters.get("from_date") and filters.get("to_date"):
+        conditions += " AND daily_sheet_date BETWEEN %(from_date)s AND %(to_date)s"
+
+    data = frappe.db.sql(f"""
+        SELECT
+            name,
+            location,
+            daily_sheet_date,
+            litres_issued_equipment
+        FROM `tabDaily Diesel Sheet`
+        {conditions}
+    """, filters, as_dict=True)
+
+    return columns, data
