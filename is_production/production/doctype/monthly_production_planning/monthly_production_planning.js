301ab729740c58b7f08262ca55959d101cfd924c
Eben van Deventer <buff0k@gmail.com>
2025-01-27 17:19:48 +0200

    modified:   is_production/production/doctype/monthly_production_days/monthly_production_days.json 	modified:   is_production/production/doctype/monthly_production_planning/monthly_production_planning.js 	modified:   is_production/production/doctype/monthly_production_planning/monthly_production_planning.json Updates

--- is_production/production/doctype/monthly_production_planning/monthly_production_planning.js
+++ is_production/production/doctype/monthly_production_planning/monthly_production_planning.js
@@ -1,93 +1,93 @@
 frappe.ui.form.on('Monthly Production Planning', {
-    refresh: function(frm) {
-        frm.add_custom_button(__('Populate Monthly Production Days'), function() {
+    refresh: function (frm) {
+        frm.add_custom_button(__('Populate Monthly Production Days'), function () {
             frm.trigger('populate_monthly_prod_days');
         }, __('Actions'));
 
-        frm.add_custom_button(__('Clear Production Days'), function() {
+        frm.add_custom_button(__('Clear Production Days'), function () {
             frm.trigger('clear_production_days');
         }, __('Actions'));
     },
 
-    populate_monthly_prod_days: function(frm) {
+    prod_month_end_date: function (frm) {
+        if (frm.doc.prod_month_end_date) {
+            let end_date = new Date(frm.doc.prod_month_end_date);
+            let last_day_of_month = new Date(end_date.getFullYear(), end_date.getMonth() + 1, 0);
+            frm.set_value('prod_month_end', frappe.datetime.obj_to_str(last_day_of_month));
+        }
+    },
+
+    populate_monthly_prod_days: function (frm) {
         try {
-            if (!frm.doc.prod_month_end) {
-                frappe.msgprint(__('Please select a valid production month end date.'));
+            if (!frm.doc.prod_month_start_date || !frm.doc.prod_month_end_date || !frm.doc.weekday_shift_hours || !frm.doc.Saturday_shift_hours || !frm.doc.num_sat_shifts) {
+                frappe.msgprint(__('Please ensure all required fields are filled: Production Start Date, Production End Date, Weekday Shift Hours, Saturday Shift Hours, and Number of Saturday Shifts.'));
                 return;
             }
 
-            let end_date = frappe.datetime.str_to_obj(frm.doc.prod_month_end);
-            if (!end_date) {
-                frappe.msgprint(__('Invalid production month end date format.'));
+            let start_date = frappe.datetime.str_to_obj(frm.doc.prod_month_start_date);
+            let end_date = frappe.datetime.str_to_obj(frm.doc.prod_month_end_date);
+
+            if (start_date > end_date) {
+                frappe.msgprint(__('The start date cannot be later than the end date.'));
                 return;
             }
-
-            let start_date = new Date(end_date.getFullYear(), end_date.getMonth(), 1);
-            let last_day = new Date(end_date.getFullYear(), end_date.getMonth() + 1, 0).getDate();
 
             frm.clear_table('month_prod_days');
-            console.log('Start Date:', start_date, 'Last Day:', last_day);
 
-            let total_day_hours = 0;
-            let total_night_hours = 0;
-            let total_morning_hours = 0;
-            let total_afternoon_hours = 0;
-
-            for (let day = 1; day <= last_day; day++) {
-                let day_date = new Date(start_date.getFullYear(), start_date.getMonth(), day);
-                let day_of_week = day_date.toLocaleDateString('en-US', { weekday: 'long' });
-
-                console.log('Processing Date:', day_date, 'Day of Week:', day_of_week);
-
-                let day_shift_hours = 0, night_shift_hours = 0;
-                let morning_shift_hours = 0, afternoon_shift_hours = 0;
+            let current_date = new Date(start_date);
+            while (current_date <= end_date) {
+                let day_of_week = current_date.toLocaleDateString('en-US', { weekday: 'long' });
+                let shift_day_hours = 0, shift_night_hours = 0;
+                let shift_morning_hours = 0, shift_afternoon_hours = 0;
 
                 if (frm.doc.shift_system === '2x12Hour') {
                     if (day_of_week === 'Saturday') {
-                        day_shift_hours = 7;
-                        night_shift_hours = 7;
+                        if (frm.doc.num_sat_shifts == 1) {
+                            shift_day_hours = frm.doc.Saturday_shift_hours || 0;
+                        } else if (frm.doc.num_sat_shifts == 2) {
+                            shift_day_hours = frm.doc.Saturday_shift_hours || 0;
+                            shift_night_hours = frm.doc.Saturday_shift_hours || 0;
+                        }
                     } else if (day_of_week === 'Sunday') {
-                        day_shift_hours = 0;
-                        night_shift_hours = 0;
+                        shift_day_hours = 0;
+                        shift_night_hours = 0;
                     } else {
-                        day_shift_hours = 9;
-                        night_shift_hours = 9;
+                        shift_day_hours = frm.doc.weekday_shift_hours || 0;
+                        shift_night_hours = frm.doc.weekday_shift_hours || 0;
                     }
                 } else if (frm.doc.shift_system === '3x8Hour') {
                     if (day_of_week === 'Saturday') {
-                        morning_shift_hours = 5;
-                        afternoon_shift_hours = 5;
-                        night_shift_hours = 5;
+                        if (frm.doc.num_sat_shifts == 1) {
+                            shift_morning_hours = frm.doc.Saturday_shift_hours || 0;
+                        } else if (frm.doc.num_sat_shifts == 2) {
+                            shift_morning_hours = frm.doc.Saturday_shift_hours || 0;
+                            shift_afternoon_hours = frm.doc.Saturday_shift_hours || 0;
+                        } else if (frm.doc.num_sat_shifts == 3) {
+                            shift_morning_hours = frm.doc.Saturday_shift_hours || 0;
+                            shift_afternoon_hours = frm.doc.Saturday_shift_hours || 0;
+                            shift_night_hours = frm.doc.Saturday_shift_hours || 0;
+                        }
                     } else if (day_of_week === 'Sunday') {
-                        morning_shift_hours = 0;
-                        afternoon_shift_hours = 0;
-                        night_shift_hours = 0;
+                        shift_morning_hours = 0;
+                        shift_afternoon_hours = 0;
+                        shift_night_hours = 0;
                     } else {
-                        morning_shift_hours = 6;
-                        afternoon_shift_hours = 6;
-                        night_shift_hours = 6;
+                        shift_morning_hours = frm.doc.weekday_shift_hours || 0;
+                        shift_afternoon_hours = frm.doc.weekday_shift_hours || 0;
+                        shift_night_hours = frm.doc.weekday_shift_hours || 0;
                     }
                 }
 
-                total_day_hours += day_shift_hours;
-                total_night_hours += night_shift_hours;
-                total_morning_hours += morning_shift_hours;
-                total_afternoon_hours += afternoon_shift_hours;
-
                 let row = frm.add_child('month_prod_days');
-                row.shift_start_date = frappe.datetime.obj_to_str(day_date);
+                row.shift_start_date = frappe.datetime.obj_to_str(current_date);
                 row.day_week = day_of_week;
-                row.shift_day_hours = day_shift_hours;
-                row.shift_night_hours = night_shift_hours;
-                row.shift_morning_hours = morning_shift_hours;
-                row.shift_afternoon_hours = afternoon_shift_hours;
-            }
+                row.shift_day_hours = shift_day_hours;
+                row.shift_night_hours = shift_night_hours;
+                row.shift_morning_hours = shift_morning_hours;
+                row.shift_afternoon_hours = shift_afternoon_hours;
 
-            frm.set_value('tot_shift_day_hours', total_day_hours);
-            frm.set_value('tot_shift_night_hours', total_night_hours);
-            frm.set_value('tot_shift_morning_hours', total_morning_hours);
-            frm.set_value('tot_shift_afternoon_hours', total_afternoon_hours);
-            frm.set_value('total_month_prod_hours', total_day_hours + total_night_hours + total_morning_hours + total_afternoon_hours);
+                current_date.setDate(current_date.getDate() + 1);
+            }
 
             frm.trigger('recalculate_totals');
             frm.refresh_field('month_prod_days');
@@ -96,6 +96,46 @@ frappe.ui.form.on('Monthly Production Planning', {
             console.error('Error in populate_monthly_prod_days:', error);
             frappe.msgprint(__('An error occurred: ' + error.message));
         }
+    },
+
+    clear_production_days: function (frm) {
+        frm.clear_table('month_prod_days');
+        frm.refresh_field('month_prod_days');
+        frm.set_value('tot_shift_day_hours', 0);
+        frm.set_value('tot_shift_night_hours', 0);
+        frm.set_value('tot_shift_morning_hours', 0);
+        frm.set_value('tot_shift_afternoon_hours', 0);
+        frm.set_value('total_month_prod_hours', 0);
+        frappe.msgprint(__('Monthly Production Days table has been cleared.'));
+    },
+
+    recalculate_totals: function (frm) {
+        let total_day_hours = 0;
+        let total_night_hours = 0;
+        let total_morning_hours = 0;
+        let total_afternoon_hours = 0;
+        let num_prod_days = 0;
+
+        frm.doc.month_prod_days.forEach(row => {
+            total_day_hours += row.shift_day_hours || 0;
+            total_night_hours += row.shift_night_hours || 0;
+            total_morning_hours += row.shift_morning_hours || 0;
+            total_afternoon_hours += row.shift_afternoon_hours || 0;
+
+            if ((row.shift_day_hours || 0) > 0 ||
+                (row.shift_night_hours || 0) > 0 ||
+                (row.shift_morning_hours || 0) > 0 ||
+                (row.shift_afternoon_hours || 0) > 0) {
+                num_prod_days++;
+            }
+        });
+
+        frm.set_value('tot_shift_day_hours', total_day_hours);
+        frm.set_value('tot_shift_night_hours', total_night_hours);
+        frm.set_value('tot_shift_morning_hours', total_morning_hours);
+        frm.set_value('tot_shift_afternoon_hours', total_afternoon_hours);
+        frm.set_value('total_month_prod_hours', total_day_hours + total_night_hours + total_morning_hours + total_afternoon_hours);
+        frm.set_value('num_prod_days', num_prod_days);
     },
 
     location: function (frm) {
@@ -194,80 +234,7 @@ frappe.ui.form.on('Monthly Production Planning', {
         }
     },
 
-    clear_production_days: function(frm) {
-        frm.clear_table('month_prod_days');
-        frm.refresh_field('month_prod_days');
-        frm.set_value('tot_shift_day_hours', 0);
-        frm.set_value('tot_shift_night_hours', 0);
-        frm.set_value('tot_shift_morning_hours', 0);
-        frm.set_value('tot_shift_afternoon_hours', 0);
-        frm.set_value('total_month_prod_hours', 0);
-        frm.set_value('num_prod_days', 0);
-        frm.set_value('target_bcm_day', 0);
-        frm.set_value('target_bcm_hour', 0);
-        frm.set_value('num_excavators', 0);
-        frm.set_value('num_trucks', 0);
-        frm.set_value('num_dozers', 0);
-        frappe.msgprint(__('Monthly Production Days table has been cleared.'));
-    },
-
-    prod_month_end: function(frm) {
-        let selected_date = frm.doc.prod_month_end;
-        if (selected_date) {
-            let date_obj = new Date(selected_date);
-            let last_day_of_month = new Date(date_obj.getFullYear(), date_obj.getMonth() + 1, 0);
-
-            if (date_obj.getDate() !== last_day_of_month.getDate()) {
-                frm.set_value('prod_month_end', frappe.datetime.obj_to_str(last_day_of_month));
-                frappe.msgprint(__('The selected date has been corrected to the last day of the month.'));
-            }
-        }
-    },
-
-    monthly_target_bcm: function(frm) {
-        frm.trigger('recalculate_totals');
-    },
-
-    recalculate_totals: function(frm) {
-        let total_day_hours = 0;
-        let total_night_hours = 0;
-        let total_morning_hours = 0;
-        let total_afternoon_hours = 0;
-        let num_prod_days = 0;
-
-        frm.doc.month_prod_days.forEach(row => {
-            total_day_hours += row.shift_day_hours || 0;
-            total_night_hours += row.shift_night_hours || 0;
-            total_morning_hours += row.shift_morning_hours || 0;
-            total_afternoon_hours += row.shift_afternoon_hours || 0;
-
-            if ((row.shift_day_hours || 0) > 0 || 
-                (row.shift_night_hours || 0) > 0 || 
-                (row.shift_morning_hours || 0) > 0 || 
-                (row.shift_afternoon_hours || 0) > 0) {
-                num_prod_days++;
-            }
-        });
-
-        let total_month_prod_hours = total_day_hours + total_night_hours + total_morning_hours + total_afternoon_hours;
-
-        frm.set_value('tot_shift_day_hours', total_day_hours);
-        frm.set_value('tot_shift_night_hours', total_night_hours);
-        frm.set_value('tot_shift_morning_hours', total_morning_hours);
-        frm.set_value('tot_shift_afternoon_hours', total_afternoon_hours);
-        frm.set_value('total_month_prod_hours', total_month_prod_hours);
-        frm.set_value('num_prod_days', num_prod_days);
-
-        if (frm.doc.monthly_target_bcm) {
-            frm.set_value('target_bcm_day', frm.doc.monthly_target_bcm / num_prod_days);
-            frm.set_value('target_bcm_hour', frm.doc.monthly_target_bcm / total_month_prod_hours);
-        } else {
-            frm.set_value('target_bcm_day', 0);
-            frm.set_value('target_bcm_hour', 0);
-        }
-    },
-
-    update_mtd_production: function(frm) {
+    update_mtd_production: function (frm) {
         if (!frm.doc.name) {
             frappe.msgprint(__('Please save the document first.'));
             return;
@@ -282,11 +249,10 @@ frappe.ui.form.on('Monthly Production Planning', {
                 },
                 fields: ['shift', 'prod_date', 'hour_total_bcm']
             },
-            callback: function(r) {
+            callback: function (r) {
                 if (r && r.message) {
                     let hourly_production_data = r.message;
-                    
-                    // Reset the BCM fields in the child table
+
                     frm.doc.month_prod_days.forEach(row => {
                         row.day_shift_bcms = 0;
                         row.night_shift_bcms = 0;
@@ -295,7 +261,6 @@ frappe.ui.form.on('Monthly Production Planning', {
                         row.total_daily_bcms = 0;
                     });
 
-                    // Process Hourly Production data
                     hourly_production_data.forEach(entry => {
                         frm.doc.month_prod_days.forEach(row => {
                             if (row.shift_start_date === frappe.datetime.obj_to_str(entry.prod_date)) {
@@ -312,7 +277,6 @@ frappe.ui.form.on('Monthly Production Planning', {
                         });
                     });
 
-                    // Calculate total_daily_bcms for each row
                     frm.doc.month_prod_days.forEach(row => {
                         row.total_daily_bcms =
                             (row.day_shift_bcms || 0) +
@@ -321,7 +285,6 @@ frappe.ui.form.on('Monthly Production Planning', {
                             (row.afternoon_shift_bcms || 0);
                     });
 
-                    // Additional calculations
                     let month_actual_bcm = frm.doc.month_prod_days.reduce((total, row) => total + (row.total_daily_bcms || 0), 0);
                     let prod_days_completed = 0;
                     let month_prod_hours_completed = 0;
@@ -331,7 +294,7 @@ frappe.ui.form.on('Monthly Production Planning', {
                         if (row.shift_start_date < today) {
                             let daily_hours = (row.shift_day_hours || 0) + (row.shift_night_hours || 0) +
                                 (row.shift_morning_hours || 0) + (row.shift_afternoon_hours || 0);
-                            
+
                             if (daily_hours > 0) {
                                 prod_days_completed++;
                                 month_prod_hours_completed += daily_hours;
@@ -366,20 +329,5 @@ frappe.ui.form.on('Monthly Production Planning', {
                 }
             }
         });
-    }
-});
-
-frappe.ui.form.on('Monthly Production Days', {
-    shift_day_hours: function(frm, cdt, cdn) {
-        frm.trigger('recalculate_totals');
-    },
-    shift_night_hours: function(frm, cdt, cdn) {
-        frm.trigger('recalculate_totals');
-    },
-    shift_morning_hours: function(frm, cdt, cdn) {
-        frm.trigger('recalculate_totals');
-    },
-    shift_afternoon_hours: function(frm, cdt, cdn) {
-        frm.trigger('recalculate_totals');
     }
 });
