c72700b9660aca5e5152707b9833f70c08a6fd99
Eben van Deventer <buff0k@gmail.com>
2025-02-20 13:50:22 +0200

    new file:   is_production/production/report/diesel_site_recon/__init__.py 	new file:   is_production/production/report/diesel_site_recon/diesel_site_recon.js 	new file:   is_production/production/report/diesel_site_recon/diesel_site_recon.json 	new file:   is_production/production/report/diesel_site_recon/diesel_site_recon.py Added Diesel Site Recon

---
+++ is_production/production/report/diesel_site_recon/diesel_site_recon.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Isambane Mining (Pty) Ltd and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Diesel Site Recon"] = {
+	"filters": [
+
+	]
+};
